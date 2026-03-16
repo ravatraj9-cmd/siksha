84841e5573edf2c6b135702d082f431cbd3c3937
ravatraj9-cmd <ravatraj9@gmail.com>
2026-03-16 18:43:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 genai.configure(api_key="AIzaSyDUO1nj1qknykSksY82SVCAW0DkowNNY1c")
 
 # Model ka ekdum sahi naam
-genai.GenerativeModel('models/gemini-1.5-flash')
+modal= genai.GenerativeModel('models/gemini-1.5-flash')
 
 st.title("🤖 Siksha AI")
 
